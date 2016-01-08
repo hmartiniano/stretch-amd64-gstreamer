dcdc3473313f1c11e82647ce0b41692b03df4b2a
Hugo Martiniano <hugomartiniano@gmail.com>
2016-01-08 18:38:55 +0000

    Switches on audio on vm. Installs all gstreamer plugins.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
-apt-get -y install build-essential libconfig-dev libgstrtspserver-1.0-dev
+apt-get -y install build-essential libconfig-dev git libgstrtspserver-1.0-dev gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
 
